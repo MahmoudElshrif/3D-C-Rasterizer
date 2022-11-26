fffd396c72fc05e6c74c6abedda203cc5929497d
Someoneyeeted <mahmoudelshrif19@gmail.com>
2022-11-26 18:57:02 +0200

    Camera controls

--- RenderTest/Camera.cpp
+++ RenderTest/Camera.cpp
@@ -11,31 +11,32 @@ Camera::Camera(float fov,float near,float far) {
 	
 }
 
-Eigen::Vector2f Camera::getProjectedPoint(Eigen::Vector3f pos) {
-	Eigen::Vector2f projected(0.f, 0.f);
+Eigen::Vector3f Camera::getProjectedPoint(Eigen::Vector3f pos) {
+	Eigen::Vector3f projected(0.f, 0.f,0.f);
 	Eigen::Vector3f transform(pos.x(), pos.y(), pos.z());
 	transform -= S2E(this->pos);
-	Eigen::Matrix3f rotz {
-		{cosf(-rot.z),sin(-rot.z),0},
-		{-sin(-rot.z),cos(-rot.z),0 },
-		{ 0,0,1 }
-	};
-	Eigen::Matrix3f roty {
+	Eigen::Matrix3f matrot{
+		{1,0,0},
+		{0,cos(-rot.x),-sin(-rot.x)},
+		{0,sin(-rot.x),cos(-rot.x)}
+	}; 
+	matrot *= Eigen::Matrix3f{
 		{cosf(-rot.y),0,sin(-rot.y)},
 		{0,1,0},
 		{-sin(-rot.y), 0 ,cos(-rot.y)}
 	};
-	Eigen::Matrix3f rotx{
-		{1,0,0},
-		{0,cos(-rot.x),sin(-rot.x)},
-		{0,-sin(-rot.x),cos(-rot.x)}
+	matrot *= Eigen::Matrix3f{
+		{cosf(-rot.z),sin(-rot.z),0},
+		{-sin(-rot.z),cos(-rot.z),0 },
+		{ 0,0,1 }
 	};
-	transform = ((rotz * roty) * rotx) * transform;
+	transform = matrot * transform;
 	//std::cout << transform << std::endl;
 	projected.x() = (near * transform.x()) / transform.z();
 	projected.y() = (near * transform.y()) / transform.z();
 	projected /= near * tanf(this->fov);
+	projected.z() = transform.z();
 	if (transform.z() <= near || transform.z() >= far)
-		return Eigen::Vector2f(5,5);
+		return Eigen::Vector3f(5,5,5);
 	return projected;
 }
